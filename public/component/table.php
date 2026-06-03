793d03fee42e1a57c9f6a1349a3f31214341e51e
MafraMP3 <caio_a_mafra@estudante.sesisenai.org.br>
2026-06-03 08:10:34 -0300

    doc - explicação dos comandos da pagina table.php

--- public/component/table.php
+++ public/component/table.php
@@ -12,10 +12,13 @@
  </tr>
 
  <?php
-
+    // define a variavel $sqlUsuarios como o resultado da instrução sql
     $sqlUsuarios = "SELECT * FROM usuario";
+
+    // transforma o $sqlusuario em um array associativo para o php entender, na forma da variavel $resultadoUsuarios.
     $resultadoUsuarios = $conn -> query($sqlUsuarios);
 
+    // percorre e exibe registros do banco de dados na forma de uma tabela
     while ($linha = $resultadoUsuarios->fetch_assoc()){
         echo"<tr>
 
